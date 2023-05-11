04851fd78986b9f36700e31845071ec5a7e97bad
Jono More <jono@imaliquiz.com>
2023-05-11 19:41:54 +0200

    unstyled Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
+  root to: 'restaurants#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all # Clear existing data
+
+restaurants = [
+  {
+    name: "Epicure",
+    address: "75008 Paris",
+    category: "french"
+  },
+  {
+    name: "Ohashi",
+    address: "Tokyo",
+    category: "japanese"
+  },
+  {
+    name: "Mama's Kitchen",
+    address: "123 Main St, New York",
+    category: "italian"
+  },
+  {
+    name: "Sichuan House",
+    address: "Beijing",
+    category: "chinese"
+  },
+  {
+    name: "Brussels Bistro",
+    address: "Brussels",
+    category: "belgian"
+  }
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create!(restaurant)
+end
